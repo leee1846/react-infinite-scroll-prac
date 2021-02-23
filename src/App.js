6dc45b2d63dfdbf164357ac2be773af93d412742
leee1846 <leee1846@gmail.com>
2021-02-23 15:48:07 +0900

    feature: 디바운싱 커스텀 훅 적용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import useBookSearch from "./useBookSearch";
 
-function App() {
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  };
+
+  const [loading, error, books, hasMore] = useBookSearch(query, pageNumber);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type='text' onChange={handleSearch} value={query} />
+      {books.map((book) => (
+        <div>{book}</div>
+      ))}
+      {loading && <div>loading...</div>}
+      {error && <div>error...</div>}
     </div>
   );
-}
+};
 
 export default App;
